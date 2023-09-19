837e9b421883c8c3366f59bf18a0e37d3f253c33
Dominic <dom.benatar@gmail.com>
2023-09-19 14:33:11 +0100

    questions added. Need to create alerts

--- app.js
+++ app.js
@@ -5,30 +5,59 @@ let fourth = true;
 let fifth = false;
 
 if (first && third && fourth) {
-  console.log("Correct, first, third and fourth are all true!")
-} else if ()
+  console.log("Correct, first, third and fourth are all true!");
+} else if (second || fifth) {
+  console.log("second and fifth were false");
+}
 
 let coffee = prompt("Do I like to drink coffee?");
 
-console.log(coffee);
-console.log(coffee.toLowerCase());
+if (coffee === "yes" || coffee === "y") {
+  console.log("Correct!");
+} else {
+  console.log("Sorry my friend, you are incorrect");
+}
+//console.log(coffee);
+//console.log(coffee.toLowerCase());
 
 let chef = prompt("Am I a classically trained chef?");
 
-console.log(chef);
-console.log(chef.toLowerCase());
+if (chef === "no" || chef === "n") {
+  console.log("Correct!");
+} else {
+  console.log("Not classically, I learnt on the job");
+}
+//console.log(chef);
+//console.log(chef.toLowerCase());
 
 let jurassic = prompt("Do I like Jurassic Park?");
+if (jurassic === "yes" || jurassic === "y") {
+  console.log("Like life, you found a way!");
+} else {
+  console.log("Try again");
+}
 
-console.log(jurassic);
-console.log(jurassic.toLowerCase());
+//console.log(jurassic);
+//console.log(jurassic.toLowerCase());
 
 let coding = prompt("Do I have any previous coding experience?");
 
-console.log(coding);
-console.log(coding.toLowerCase());
+if (coding === "yes" || coding === "y") {
+  console.log("I have a little bit of experience.");
+} else {
+  console.log("I dont have much but I do have some experience");
+}
+
+//console.log(coding)
+//console.log(coding.toLowerCase());
 
 let pineapple = prompt("Should pineapple go on pizza?");
 
-console.log(pineapple);
-console.log(pineapple.toLowerCase());
+if (pineapple === "no" || pineapple === "n") {
+  console.log("Get in the bin!");
+} else {
+  console.log("If you think yes, then im sorry. Get in the bin");
+}
+
+//console.log(pineapple);
+//console.log(pineapple.toLowerCase());
