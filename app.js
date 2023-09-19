85d3080827aaf759723f1a7ec6136fdbc98cf213
Dominic <dom.benatar@gmail.com>
2023-09-19 15:38:44 +0100

    prompts and alerts done. Adding button function.

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+let user = prompt("Hello, what is your name?");
+alert("Hello " + user + ", welcome");
+document.write("Hello " + user + ", welcome");
+
 let first = true;
 let second = false;
 let third = true;
@@ -10,65 +14,66 @@ if (first && third && fourth) {
   console.log("second and fifth were false");
 }
 
-let coffee = prompt("Do I like to drink coffee?");
+function quizzical() {
+  let coffee = prompt("Do I like to drink coffee?");
 
-if (coffee === "yes" || coffee === "y") {
-  alert("Correct!");
-  //console.log("Correct!");
-} else {
-  alert("Sorry my friend, my blood is mostly caffeine");
-  //console.log("Sorry my friend, you are incorrect");
+  if (coffee === "yes" || coffee === "y") {
+    alert("Correct!");
+    //console.log("Correct!");
+  } else {
+    alert("Sorry my friend, my blood is mostly caffeine");
+    //console.log("Sorry my friend, you are incorrect");
+  }
+  //console.log(coffee);
+  //console.log(coffee.toLowerCase());
+
+  let chef = prompt("Am I a classically trained chef?");
+
+  if (chef === "no" || chef === "n") {
+    alert("Correct!");
+    //console.log("Correct!");
+  } else {
+    alert("YES CHEF! I mean no, im not classically trained.");
+    //console.log("Not classically, I learnt on the job");
+  }
+  //console.log(chef);
+  //console.log(chef.toLowerCase());
+
+  let jurassic = prompt("Do I like Jurassic Park?");
+
+  if (jurassic === "yes" || jurassic === "y") {
+    alert("Like life, you found a way!");
+    //console.log("Like life, you found a way!");
+  } else {
+    alert("Try again");
+    //console.log("Try again");
+  }
+
+  //console.log(jurassic);
+  //console.log(jurassic.toLowerCase());
+
+  let coding = prompt("Do I have any previous coding experience?");
+
+  if (coding === "yes" || coding === "y") {
+    alert("I have a little bit of experience.");
+    //console.log("I have a little bit of experience.");
+  } else {
+    alert("I dont have much but I do have some experience");
+    //console.log("I dont have much but I do have some experience");
+  }
+
+  //console.log(coding)
+  //console.log(coding.toLowerCase());
+
+  let pineapple = prompt("Should pineapple go on pizza?");
+
+  if (pineapple === "no" || pineapple === "n") {
+    alert("Get in the bin!");
+    //console.log("Get in the bin!");
+  } else {
+    alert("If you think yes, then im sorry. Get in the bin");
+    //console.log("If you think yes, then im sorry. Get in the bin");
+  }
 }
-//console.log(coffee);
-//console.log(coffee.toLowerCase());
-
-let chef = prompt("Am I a classically trained chef?");
-
-if (chef === "no" || chef === "n") {
-  alert("Correct!");
-  //console.log("Correct!");
-} else {
-  alert("YES CHEF! I mean no, im not classically trained.");
-  //console.log("Not classically, I learnt on the job");
-}
-//console.log(chef);
-//console.log(chef.toLowerCase());
-
-let jurassic = prompt("Do I like Jurassic Park?");
-
-if (jurassic === "yes" || jurassic === "y") {
-  alert("Like life, you found a way!");
-  //console.log("Like life, you found a way!");
-} else {
-  alert("Try again");
-  //console.log("Try again");
-}
-
-//console.log(jurassic);
-//console.log(jurassic.toLowerCase());
-
-let coding = prompt("Do I have any previous coding experience?");
-
-if (coding === "yes" || coding === "y") {
-  alert("I have a little bit of experience.");
-  //console.log("I have a little bit of experience.");
-} else {
-  alert("I dont have much but I do have some experience");
-  //console.log("I dont have much but I do have some experience");
-}
-
-//console.log(coding)
-//console.log(coding.toLowerCase());
-
-let pineapple = prompt("Should pineapple go on pizza?");
-
-if (pineapple === "no" || pineapple === "n") {
-  alert("Get in the bin!");
-  //console.log("Get in the bin!");
-} else {
-  alert("If you think yes, then im sorry. Get in the bin");
-  //console.log("If you think yes, then im sorry. Get in the bin");
-}
-
 //console.log(pineapple);
 //console.log(pineapple.toLowerCase());
