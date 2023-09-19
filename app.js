de6659e707602e30a7c13479e27f10359de530d5
Dominic <dom.benatar@gmail.com>
2023-09-19 12:00:32 +0100

    added but havent finished else if

---
+++ app.js
@@ -0,0 +1,34 @@
+let first = true;
+let second = false;
+let third = true;
+let fourth = true;
+let fifth = false;
+
+if (first && third && fourth) {
+  console.log("Correct, first, third and fourth are all true!")
+} else if ()
+
+let coffee = prompt("Do I like to drink coffee?");
+
+console.log(coffee);
+console.log(coffee.toLowerCase());
+
+let chef = prompt("Am I a classically trained chef?");
+
+console.log(chef);
+console.log(chef.toLowerCase());
+
+let jurassic = prompt("Do I like Jurassic Park?");
+
+console.log(jurassic);
+console.log(jurassic.toLowerCase());
+
+let coding = prompt("Do I have any previous coding experience?");
+
+console.log(coding);
+console.log(coding.toLowerCase());
+
+let pineapple = prompt("Should pineapple go on pizza?");
+
+console.log(pineapple);
+console.log(pineapple.toLowerCase());
